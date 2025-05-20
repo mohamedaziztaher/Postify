3def2ac0f80088dc6297277412f7cf33571d4abf
Taher Mohamed Aziz <mohamedaziztaher@gmail.com>
2025-05-20 16:46:40 +0100

    Refactor code structure for improved readability and maintainability

--- app/src/main/java/epi/gl4c/finalversion/MyApp.kt
+++ app/src/main/java/epi/gl4c/finalversion/MyApp.kt
@@ -2,15 +2,17 @@ package epi.gl4c.finalversion
 
 import android.app.Application
 import com.cloudinary.android.MediaManager
+import java.util.HashMap
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        val cloudinaryConfig = mapOf(
-            "cloud_name" to "dip6whhie",
-            "api_key" to "742521121998967",
-            "api_secret" to "yrKK6LSpsjNMz9_yBoDP5hwUT9E"
-        )
-        MediaManager.init(this, cloudinaryConfig)
+        
+        // Initialize Cloudinary
+        val config = HashMap<String, String>()
+        config["cloud_name"] = "dip6whhie"
+        config["api_key"] = "742521121998967"
+        config["api_secret"] = "yrKK6LSpsjNMz9_yBoDP5hwUT9E"
+        MediaManager.init(this, config)
     }
 }
